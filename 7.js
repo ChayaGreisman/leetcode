0f4ca677e7ae84ee00e1602bb48183e5ee7245ac
ChayaGreisman <chayamgreisman@gmail.com>
2020-10-11 19:25:32 -0700

    another solution

--- 7.js
+++ 7.js
@@ -21,4 +21,22 @@ var reverse = function(x) {
     return absReversed * Math.sign(x);
 };
 
-//uses .reverse() .....?
+//^^uses .reverse() .....?
+
+var reverse = function(x){
+    if (x < 0) return -reverse(-x);
+
+    var reversedInt = 0;
+
+    while(x>0) {
+        var a = x%10;
+        x = Math.floor(x/10);
+
+        //Should no larger than Math.floor(Number.MAX_VALUE/10)
+        if(reversedInt >= 214748365)
+            return 0;
+        reversedInt = reversedInt*10+a;
+    }
+
+    return reversedInt;
+}
