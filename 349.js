28244dd077fadf4d7bbb325403115d137e78980f
ChayaGreisman <chayamgreisman@gmail.com>
2020-10-20 13:32:16 -0700

    more solutions

--- 349.js
+++ 349.js
@@ -32,3 +32,44 @@ function intersect(nums1, nums2){
 
     return result;
 }
+
+
+
+
+//Brute Force Solution: O(n logn)
+
+function intersect(nums1, nums2){
+    let sortedNums1 = nums1.sort((a , b) => a - b);
+        let sortedNums2 = nums2.sort((a , b) => a - b);
+        let result = [];
+        let i = 0;
+        let j = 0;
+        
+        while(i < sortedNums1.length && j < sortedNums2.length){
+            if(sortedNums1[i] === sortedNums2[j]){
+                if(!result.includes(sortedNums1[i])) result.push(sortedNums1[i]);
+                i++;
+                j++;
+            }
+            else if(sortedNums1[i] < sortedNums2[j]) i++;
+            else j++;
+        }
+        return result;
+    }
+    //HashMap Solution: O(n)
+    
+    function intersect(nums1, nums2){
+        let map = new Map();
+        for(let num of nums1){
+            if(!map.has(num))
+                map.set(num, 1);
+        }
+        
+        return nums2.filter(n => {
+            if(map.has(n)){
+                map.delete(n);
+                return true;
+            }
+            else return false;
+        });
+    }
