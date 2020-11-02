5972ce46b3e76a7058a5557335d6dc11873e1586
ChayaGreisman <chayamgreisman@gmail.com>
2020-11-01 20:16:57 -0500

    first unique character in string

--- 387.js
+++ 387.js
@@ -8,3 +8,24 @@
 // return 2.
  
 // Note: You may assume the string contains only lowercase English letters.
+
+var firstUniqChar = function(s) {
+    let map = {}
+    
+    for (let char of s) {
+        map[char] ? map[char]++ : map[char] = 1
+    }
+    
+    for (let i = 0; i < s.length; i++) {
+        if (map[s[i]] === 1) return i
+    }
+        
+    return -1
+};
+
+
+var firstUniqChar = function(s) {
+    for(i=0; i<s.length; i++)
+        if(s.indexOf(s[i])===s.lastIndexOf(s[i])) return i
+    return -1
+};
